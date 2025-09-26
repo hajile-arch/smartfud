32314c02b5d631cce793a39904a88a1fdd6435e7
Vishva2183 <108569411+Vishva2183@users.noreply.github.com>
2025-09-26 11:48:18 +0800

    Created new files for Convert Donation and Donation Items

--- frontend/src/FoodInventory.jsx
+++ frontend/src/FoodInventory.jsx
@@ -1,1 +1,13 @@
-<p>Page for food inventory Smartfud</p>
+import React, { useState, useEffect } from "react";
+import { auth, db } from "./firebase";
+import {
+  collection,
+  addDoc,
+  query,
+  where,
+  onSnapshot,
+  doc,
+  updateDoc,
+  deleteDoc,
+  orderBy,
+} from "firebase/firestore";
